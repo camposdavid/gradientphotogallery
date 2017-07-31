bf983e3efcb20554735ec66885081b960785ddee
David Campos <David Campos>
2017-07-30 19:04:30 -0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+new WOW().init();
+
+// MDB Lightbox Init
+$(function () {
+    $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
+
+
+function initialize() {
+	var mapProp = {
+		center: new google.maps.LatLng(37.788890, -122.398104),
+		zoom: 12, 
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	var map = new google.maps.Map(document.getElementById("map-container"), mapProp)
+}
+google.maps.event.addDomListener(window, "load", initialize)
+
